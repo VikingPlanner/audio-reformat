fdd33dc69df769b108b5fd3740f503d00e546067
VikingPlanner <86377262+VikingPlanner@users.noreply.github.com>
2026-06-16 20:32:47 +0300

    Update PREOBRAZOVATOR.py

--- PREOBRAZOVATOR.py
+++ PREOBRAZOVATOR.py
@@ -1,8 +1,8 @@
 import pydub
 
-def preobrazovator(filename, export_name, format):
-    obj = pydub.AudioSegment.from_file(f'{filename}', format=f'{format}')
-    obj.export(f'{export_name}', format='mp3')
+def preobrazovator(filename, export_name, input_format, export_format):
+    obj = pydub.AudioSegment.from_file(f'{filename}', format=f'{input_format}')
+    obj.export(f'{export_name}', format=f'{export_format}')
 
 #preobrazovator('Gats.m4a', 'Gats.mp3', 'm4a')
 
